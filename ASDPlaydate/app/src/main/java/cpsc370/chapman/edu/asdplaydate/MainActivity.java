1b8235b00063bb246b123a848f7334c4572a0929
rburns <rburns@chapman.edu>
2015-11-02 20:28:08 -0800

    initial build of project

---
+++ ASDPlaydate/app/src/main/java/cpsc370/chapman/edu/asdplaydate/MainActivity.java
@@ -0,0 +1,145 @@
+package cpsc370.chapman.edu.asdplaydate;
+
+import java.util.Locale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import cpsc370.chapman.edu.asdplaydate.fragments.FindFragment;
+import cpsc370.chapman.edu.asdplaydate.fragments.InboxFragment;
+import cpsc370.chapman.edu.asdplaydate.fragments.ResultListFragment;
+
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener
+{
+    SectionsPagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
+        {
+            @Override
+            public void onPageSelected(int position)
+            {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++)
+        {
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
+    {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
+    {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
+    {
+    }
+
+    public void settings(MenuItem item)
+    {
+        //ISAAC HI
+    }
+
+    public void chat(MenuItem item)
+    {
+        //MARTIN HI
+    }
+
+    public void account(MenuItem item)
+    {
+        //GARRETT HI
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter
+    {
+        public SectionsPagerAdapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            Fragment f;
+            switch (position)
+            {
+                case 0:
+                    f = new FindFragment();
+                    break;
+                case 1:
+                    f = new InboxFragment();
+                    break;
+                default:
+                    f = new ResultListFragment();
+                    break;
+            }
+            return f;
+        }
+
+        @Override
+        public int getCount()
+        {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            Locale locale = Locale.getDefault();
+            switch (position)
+            {
+                case 0:
+                    return getString(R.string.find).toUpperCase(locale);
+                case 1:
+                    return getString(R.string.chat).toUpperCase(locale);
+                default:
+                    return "LIST";
+            }
+        }
+    }
+}
