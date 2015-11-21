200d7d511f3acac929f9e21640ae5f3e8939d8a7
Kelly Ochikubo <ochik100@mail.chapman.edu>
2015-11-20 23:10:00 -0800

    retrieves profile info in settings - missing gender attribute

--- ASDPlaydate/app/src/main/java/edu/chapman/cpsc370/asdplaydate/activities/ProfileActivity.java
+++ ASDPlaydate/app/src/main/java/edu/chapman/cpsc370/asdplaydate/activities/ProfileActivity.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
 
@@ -34,6 +35,9 @@ public class ProfileActivity extends AppCompatActivity
     private Child child;
     private ProgressDialog progressDialog;
     String parentFirst, parentLast, city, childName, age, condition, gender;
+    EditText parentFirstName, parentLastName, cityName, childAge, childFirst, childCondition;
+    RadioButton childGenderButton;
+    RadioGroup childGenderGroup;
 
     SessionManager sessionManager;
 
@@ -45,52 +49,100 @@ public class ProfileActivity extends AppCompatActivity
 
         sessionManager = new SessionManager(getApplicationContext());
 
-        //Find Views By ID
-        FloatingActionButton saveProfileButton = (FloatingActionButton) findViewById(R.id.fab_saveProfile);//find list view
+        parentFirstName = (EditText) findViewById(R.id.editParentFname);
+        parentLastName = (EditText) findViewById(R.id.editParentLname);
+        cityName = (EditText) findViewById(R.id.editCity);
+        childFirst = (EditText) findViewById(R.id.editChildFname);
+        childAge = (EditText) findViewById(R.id.editAge);
+        childCondition = (EditText) findViewById(R.id.editConditionInformation);
+        childGenderGroup = (RadioGroup) findViewById(R.id.radioGroupGender);
+
+
+        FloatingActionButton saveProfileButton = (FloatingActionButton) findViewById(R.id.fab_saveProfile);
+        saveProfileButton.setOnClickListener(onClickListener);
         final TextView defaultSearchRadius = (TextView) findViewById(R.id.textViewSearchRadius);
 
-        //Click Listeners
-        saveProfileButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v)
+        if(ASDPlaydateUser.getCurrentUser() != null)
+        {
+            try
             {
-                // Save currently entered info
-                getCurrentInfo();
-
-                // Check if a new account is being created
-                if (getIntent().getStringExtra(CreateAccountFragment.ACCOUNT_EMAIL) != null)
-                {
-                    createAccount();
-                }
-                else
-                {
-                    updateProfile();
-                }
+                getProfileInfo();
             }
-        });
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+
     }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            // Save currently entered info
+            getCurrentInfo();
+
+            // Check if a new account is being created
+            if (getIntent().getStringExtra(CreateAccountFragment.ACCOUNT_EMAIL) != null)
+            {
+                createAccount();
+            }
+            else
+            {
+                updateProfile();
+            }
+        }
+    };
+
+    private void getProfileInfo() throws Exception
+    {
+        ASDPlaydateUser currentUser = (ASDPlaydateUser)ASDPlaydateUser.getCurrentUser();
+        if(currentUser != null)
+        {
+            parentFirstName.setText(currentUser.getFirstName());
+            parentLastName.setText(currentUser.getLastName());
+            cityName.setText(currentUser.getCityName());
+
+            Child currentChild = getChildWithParent(currentUser);
+            if(currentChild != null)
+            {
+                childFirst.setText(currentChild.getFirstName());
+                childAge.setText(String.valueOf(currentChild.getAge()));
+                childCondition.setText(currentChild.getDescription());
+
+            }
+        }
+
+
+    }
+
+    private Child getChildWithParent(ASDPlaydateUser parent) throws Exception
+    {
+        ParseQuery<Child> q = new ParseQuery<Child>(Child.class);
+        q.whereEqualTo(Child.ATTR_PARENT, parent);
+        return q.find().get(0);
+    }
+
 
     private void getCurrentInfo()
     {
 
-        EditText parentFirstName = (EditText) findViewById(R.id.editParentFname);
         parentFirst = parentFirstName.getText().toString();
-        EditText parentLastName = (EditText) findViewById(R.id.editParentLname);
         parentLast = parentLastName.getText().toString();
-        EditText cityName = (EditText) findViewById(R.id.editCity);
         city = cityName.getText().toString();
-        EditText childFirst = (EditText) findViewById(R.id.editChildFname);
         childName = childFirst.getText().toString();
-        EditText childAge = (EditText) findViewById(R.id.editAge);
         age = childAge.getText().toString();
-        EditText childCondition = (EditText) findViewById(R.id.editConditionInformation);
         condition = childCondition.getText().toString();
-        RadioGroup childGenderGroup = (RadioGroup) findViewById(R.id.radioGroupGender);
+
 
         // Get the selected gender
         Integer selectedId = childGenderGroup.getCheckedRadioButtonId();
         if(selectedId != -1)
         {
-            RadioButton childGenderButton = (RadioButton) findViewById(selectedId);
+            childGenderButton = (RadioButton) findViewById(selectedId);
             gender = childGenderButton.getText().toString().toUpperCase();
         }
         else
