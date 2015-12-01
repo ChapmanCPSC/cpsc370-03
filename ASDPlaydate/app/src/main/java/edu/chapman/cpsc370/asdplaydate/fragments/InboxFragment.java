40e65a962985671b65c4361205e520f7a646bff6
Martin Caestecker <caest100@mail.chapman.edu>
2015-12-01 14:09:24 -0800

    pull down to refresh conversations in inbox

--- ASDPlaydate/app/src/main/java/edu/chapman/cpsc370/asdplaydate/fragments/InboxFragment.java
+++ ASDPlaydate/app/src/main/java/edu/chapman/cpsc370/asdplaydate/fragments/InboxFragment.java
@@ -2,6 +2,7 @@ package edu.chapman.cpsc370.asdplaydate.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -19,6 +20,7 @@ import org.joda.time.DateTimeZone;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Handler;
 
 import edu.chapman.cpsc370.asdplaydate.adapters.ChatRequestListRecyclerAdapter;
 import edu.chapman.cpsc370.asdplaydate.managers.SessionManager;
@@ -27,6 +29,7 @@ import edu.chapman.cpsc370.asdplaydate.models.ChatRequestListRecyclerItem;
 import edu.chapman.cpsc370.asdplaydate.R;
 import edu.chapman.cpsc370.asdplaydate.SwipeableRecyclerViewTouchListener;
 import edu.chapman.cpsc370.asdplaydate.models.Conversation;
+import edu.chapman.cpsc370.asdplaydate.tasks.GetMessagesTask;
 
 public class InboxFragment extends Fragment
 {
@@ -35,21 +38,10 @@ public class InboxFragment extends Fragment
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private Button btn_refresh;
+    private SwipeRefreshLayout swipeLayout;
 
     public InboxFragment()
     {
-        /*ASDPlaydateUser currentUser = (ASDPlaydateUser) ASDPlaydateUser.getCurrentUser();
-
-        if (!currentUser.equals(null)){
-            //Toast.makeText(getActivity(), "current user is ok", Toast.LENGTH_SHORT).show();
-            try
-            {
-                getConversations();
-            } catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-        }*/
     }
 
     @Override
@@ -128,10 +120,45 @@ public class InboxFragment extends Fragment
     {
         super.onViewCreated(view, savedInstanceState);
 
+        swipeLayout = (SwipeRefreshLayout) getActivity().findViewById(R.id.swipe_container);
         mRecyclerView = (RecyclerView) getActivity().findViewById(R.id.rv_chatrequestlist);
-        btn_refresh = (Button) getActivity().findViewById(R.id.btn_refresh);
+        //btn_refresh = (Button) getActivity().findViewById(R.id.btn_refresh);
 
-        btn_refresh.setOnClickListener(new View.OnClickListener()
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener()
+        {
+            @Override
+            public void onRefresh()
+            {
+                final GetMessagesTask task = new GetMessagesTask(getActivity());
+                task.onFinish = new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        mItems.clear();
+
+                        for (ChatRequestListRecyclerItem item : task.mItems)
+                        {
+                            mItems.add(item);
+                        }
+
+                        if (mItems.size() == 0)
+                        {
+                            Toast.makeText(getActivity(), getActivity().getResources().getText(R.string.no_convos_found), Toast.LENGTH_SHORT).show();
+                        }
+
+                        mAdapter.notifyDataSetChanged();
+                        swipeLayout.setRefreshing(false);
+                    }
+                };
+
+                task.execute();
+
+
+            }
+        });
+
+        /*btn_refresh.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
@@ -145,7 +172,7 @@ public class InboxFragment extends Fragment
                 }
 
             }
-        });
+        });*/
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -154,17 +181,6 @@ public class InboxFragment extends Fragment
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
-
-        // specify an adapter (see also next example)
-        //TODO: Get data from somewhere here
-
-
-        //Martin 11/8/15 added userID to recycler item for future use by chat activity
-        /*mItems.add(new ChatRequestListRecyclerItem(1234, "John Smith", "I have a red Angels hat on", true));
-        mItems.add(new ChatRequestListRecyclerItem(1235, "Carry Johnson", "Would like to chat", false));
-        mItems.add(new ChatRequestListRecyclerItem(1236, "Faia Raige", "That is awesome!", true));
-        mItems.add(new ChatRequestListRecyclerItem(1237, "Chris Shiherlis ", "Would like to chat", false));
-        mItems.add(new ChatRequestListRecyclerItem(1238, "Terrence Fletcher", "Were you dragging or rushing?", true));*/
 
         mAdapter = new ChatRequestListRecyclerAdapter(mItems, this.getActivity());
         mRecyclerView.setAdapter(mAdapter);
---
+++ ASDPlaydate/app/src/main/java/edu/chapman/cpsc370/asdplaydate/tasks/GetMessagesTask.java
@@ -0,0 +1,113 @@
+package edu.chapman.cpsc370.asdplaydate.tasks;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.parse.ParseQuery;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.chapman.cpsc370.asdplaydate.managers.SessionManager;
+import edu.chapman.cpsc370.asdplaydate.models.ASDPlaydateUser;
+import edu.chapman.cpsc370.asdplaydate.models.ChatRequestListRecyclerItem;
+import edu.chapman.cpsc370.asdplaydate.models.Conversation;
+
+/**
+ * Created by Martin on 12/1/2015.
+ */
+public class GetMessagesTask extends AsyncTask<Void, Void, Void>
+{
+    Context ctx;
+    public Runnable onFinish;
+    public List<ChatRequestListRecyclerItem> mItems = new ArrayList<>();
+
+    public GetMessagesTask(Context ctx)
+    {
+        this.ctx = ctx;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params)
+    {
+        try
+        {
+            //receiver from testSendChatInvitation()
+            SessionManager sessionManager = new SessionManager(ctx);
+            ASDPlaydateUser me = (ASDPlaydateUser) ASDPlaydateUser.become(sessionManager.getSessionToken());
+
+            ParseQuery<Conversation> meReceive = new ParseQuery<>(Conversation.class);
+            meReceive.whereEqualTo(Conversation.ATTR_RECEIVER, me);
+
+            ParseQuery<Conversation> meSend = new ParseQuery<>(Conversation.class);
+            meSend.whereEqualTo(Conversation.ATTR_INITIATOR, me);
+
+            List<ParseQuery<Conversation>> both = new ArrayList<>();
+            both.add(meReceive);
+            both.add(meSend);
+
+            ParseQuery<Conversation> meSendOrMeReceive = ParseQuery.or(both);
+            meSendOrMeReceive.whereNotEqualTo(Conversation.ATTR_STATUS, Conversation.Status.DENIED.name());
+            meSendOrMeReceive.whereGreaterThan(Conversation.ATTR_EXPIRE_DATE, DateTime.now(DateTimeZone.UTC).toDate());
+
+            List<Conversation> displayConvos = meSendOrMeReceive.find();
+
+            mItems.clear();
+            for (Conversation c : displayConvos)
+            {
+                boolean accepted = false;
+                if (c.getStatus().toString().equals(Conversation.Status.ACCEPTED.name()))
+                    accepted = true;
+
+                ASDPlaydateUser initiator = c.getInitiator();
+                ASDPlaydateUser receiver = c.getReceiver();
+
+                if (initiator.equals(me))
+                {
+                    String firstName = "";
+                    String lastName = "";
+                    try
+                    {
+                        firstName = receiver.fetchIfNeeded().getString("first_name");
+                        lastName = receiver.fetchIfNeeded().getString("last_name");
+                    } catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                    mItems.add(new ChatRequestListRecyclerItem(c.getObjectId(), receiver.getObjectId(), firstName + " " + lastName, null, accepted));
+                } else
+                {
+                    String firstName = "";
+                    String lastName = "";
+                    try
+                    {
+                        firstName = initiator.fetchIfNeeded().getString("first_name");
+                        lastName = initiator.fetchIfNeeded().getString("last_name");
+                    } catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                    mItems.add(new ChatRequestListRecyclerItem(c.getObjectId(), initiator.getObjectId(), firstName + " " + lastName, null, accepted));
+                }
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void params)
+    {
+        super.onPostExecute(params);
+
+        if (onFinish != null)
+        {
+            onFinish.run();
+        }
+    }
+}
