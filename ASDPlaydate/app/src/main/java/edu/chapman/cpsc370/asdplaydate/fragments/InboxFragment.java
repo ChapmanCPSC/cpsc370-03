4fcba36e6090999e6a5285b56c4b3ff2fbf40e6b
Martin Caestecker <caest100@mail.chapman.edu>
2015-12-02 19:40:13 -0800

    remove convo count toast

--- ASDPlaydate/app/src/main/java/edu/chapman/cpsc370/asdplaydate/fragments/InboxFragment.java
+++ ASDPlaydate/app/src/main/java/edu/chapman/cpsc370/asdplaydate/fragments/InboxFragment.java
@@ -75,12 +75,6 @@ public class InboxFragment extends Fragment
                 if (mItems.size() == 0)
                 {
                     Toast.makeText(getActivity(), getActivity().getResources().getText(R.string.no_convos_found), Toast.LENGTH_SHORT).show();
-                } else
-                {
-                    if (BaseApplication.inDEBUGMode())
-                    {
-                        Toast.makeText(getActivity(), mItems.size() + " convos in Inbox", Toast.LENGTH_SHORT).show();
-                    }
                 }
 
                 mAdapter.notifyDataSetChanged();
