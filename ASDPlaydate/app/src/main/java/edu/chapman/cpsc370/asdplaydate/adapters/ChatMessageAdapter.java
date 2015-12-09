c26d083eb6bfee67d163ae13dac7b0f90cb839ae
Martin Caestecker <caest100@mail.chapman.edu>
2015-12-08 21:05:28 -0800

    Update to dynamically alter chat message bubbles

--- ASDPlaydate/app/src/main/java/edu/chapman/cpsc370/asdplaydate/adapters/ChatMessageAdapter.java
+++ ASDPlaydate/app/src/main/java/edu/chapman/cpsc370/asdplaydate/adapters/ChatMessageAdapter.java
@@ -1,37 +1,27 @@
 package edu.chapman.cpsc370.asdplaydate.adapters;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeFormatterBuilder;
-
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
 import edu.chapman.cpsc370.asdplaydate.R;
 import edu.chapman.cpsc370.asdplaydate.models.ASDPlaydateUser;
-import edu.chapman.cpsc370.asdplaydate.models.ChatMessage;
 import edu.chapman.cpsc370.asdplaydate.models.Message;
 
-/**
- * Created by Martin on 11/3/2015.
- */
 public class ChatMessageAdapter extends ArrayAdapter<Message>
 {
     Context context;
-    LinearLayout ll_bubbleContainer;
     ASDPlaydateUser sender;
 
     public ChatMessageAdapter(Context context, List<Message> messages, ASDPlaydateUser sender)
     {
-        super(context, R.layout.row_message_me, messages);
+        super(context, R.layout.row_message, messages);
         this.context = context;
         this.sender = sender;
     }
@@ -41,23 +31,31 @@ public class ChatMessageAdapter extends ArrayAdapter<Message>
     {
         Message message = getItem(position);
         LayoutInflater inflater = LayoutInflater.from(context);
-        View rootView;
 
-        if(sender.equals(message.getAuthor()))
-        {
-            rootView = inflater.inflate(R.layout.row_message_me, parent, false);
-        }
-        else
-        {
-            rootView = inflater.inflate(R.layout.row_message_notme, parent, false);
-        }
-
+        View rootView = inflater.inflate(R.layout.row_message, parent, false);
         TextView tv_message = (TextView) rootView.findViewById(R.id.tv_Message);
         TextView tv_dateTime = (TextView) rootView.findViewById(R.id.tv_dateTime);
-        ll_bubbleContainer = (LinearLayout) rootView.findViewById(R.id.ll_bubbleContainer);
+        LinearLayout ll_messageRowContainer = (LinearLayout) rootView.findViewById(R.id.ll_messageRowContainer);
+        LinearLayout ll_bubbleContainer = (LinearLayout) rootView.findViewById(R.id.ll_bubbleContainer);
 
         tv_message.setText(message.getText());
         tv_dateTime.setText(message.getTimestamp().toString("hh:mm a, MM-dd-yyyy"));
+        LinearLayout.LayoutParams llParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        llParams.setMargins(10, 10, 10, 2);
+        if(sender.equals(message.getAuthor()))
+        {
+            llParams.gravity = Gravity.END;
+            ll_bubbleContainer.setBackgroundResource(R.drawable.msgbox_me);
+            ll_messageRowContainer.setGravity(Gravity.END);
+        }
+        else
+        {
+            llParams.gravity = Gravity.START;
+            ll_bubbleContainer.setBackgroundResource(R.drawable.msgbox_notme);
+            ll_messageRowContainer.setGravity(Gravity.START);
+        }
+
+        ll_bubbleContainer.setLayoutParams(llParams);
 
         return rootView;
     }
